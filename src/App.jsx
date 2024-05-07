2263bbd43fd3c624c08894ae9c68765a03f91e42
ayush931 <159576275+ayush931@users.noreply.github.com>
2024-05-07 18:17:17 +0530

    Added denied page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom'
 import AboutUs from './Pages/AboutUs'
 import Contact from './Pages/Contact'
 import CourseList from './Pages/Course/CourseList'
+import Denied from './Pages/Denied'
 import HomePage from './Pages/HomePage'
 import Login from './Pages/Login'
 import NotFound from './Pages/NotFound'
@@ -20,6 +21,7 @@ function App() {
         <Route path='/about' element={<AboutUs />}></Route>
         <Route path='/courses' element={<CourseList />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/denied' element={<Denied />} />
 
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
